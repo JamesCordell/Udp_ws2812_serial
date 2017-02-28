30524cd54df3e03e1f99a100ad7c1e0ccbb0d3a9
jcordell <jcordell@jcordell-Lenovo-YOGA-700-14ISK>
2017-02-28 21:37:17 +0000

    added files

---
+++ MasterArduino555MainRoom/MasterArduino555MainRoom.ino
@@ -0,0 +1,371 @@
+/*
+ * UIPEthernet UdpServer example.
+ *
+ * UIPEthernet is a TCP/IP stack that can be used with a enc28j60 based
+ * Ethernet-shield.
+ *
+ * UIPEthernet uses the fine uIP stack by Adam Dunkels <adam@sics.se>
+ *
+ *      -----------------
+ *
+ * This UdpServer example sets up a udp-server at 192.168.0.6 on port 5000.
+ * send packet via upd to test
+ *
+ * Copyright (C) 2013 by Norbert Truchsess (norbert.truchsess@t-online.de)
+ */
+ 
+#include <UIPEthernet.h>
+#include <util/delay.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIXELS 555  // Number of pixels in the string
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products
+ 
+#define PIXEL_PORT  PORTD  // Port of the pin the pixels are connected to
+#define PIXEL_DDR   DDRD   // Port of the pin the pixels are connected to
+#define PIXEL_BIT   5      // Bit of the pin the pixels are connected to
+
+// These are the timing constraints taken mostly from the WS2812 datasheets 
+// These are chosen to be conservative and avoid problems rather than for maximum throughput 
+
+#define T1H  900    // Width of a 1 bit in ns
+#define T1L  600    // Width of a 1 bit in ns
+
+#define T0H  400    // Width of a 0 bit in ns
+#define T0L  900    // Width of a 0 bit in ns
+
+#define RES 6000    // Width of the low gap between bits to cause a frame to latch
+
+// Here are some convience defines for using nanoseconds specs to generate actual CPU delays
+
+#define NS_PER_SEC (1000000000L)          // Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
+
+#define CYCLES_PER_SEC (F_CPU)
+
+#define NS_PER_CYCLE ( NS_PER_SEC / CYCLES_PER_SEC )
+
+#define NS_TO_CYCLES(n) ( (n) / NS_PER_CYCLE )
+ 
+ 
+
+
+
+//############################################################################
+
+
+
+inline void sendBit( bool bitVal ) {
+  
+    if (  bitVal ) {				// 0 bit
+      
+		asm volatile (
+			"sbi %[port], %[bit] \n\t"				// Set the output bit
+			".rept %[onCycles] \n\t"                                // Execute NOPs to delay exactly the specified number of cycles
+			"nop \n\t"
+			".endr \n\t"
+			"cbi %[port], %[bit] \n\t"                              // Clear the output bit
+			".rept %[offCycles] \n\t"                               // Execute NOPs to delay exactly the specified number of cycles
+			"nop \n\t"
+			".endr \n\t"
+			::
+			[port]		"I" (_SFR_IO_ADDR(PIXEL_PORT)),
+			[bit]		"I" (PIXEL_BIT),
+			[onCycles]	"I" (NS_TO_CYCLES(T1H) - 2),		// 1-bit width less overhead  for the actual bit setting, note that this delay could be longer and everything would still work
+			[offCycles] 	"I" (NS_TO_CYCLES(T1L) - 2)			// Minimum interbit delay. Note that we probably don't need this at all since the loop overhead will be enough, but here for correctness
+
+		);
+                                  
+    } else {					// 1 bit
+
+		// **************************************************************************
+		// This line is really the only tight goldilocks timing in the whole program!
+		// **************************************************************************
+
+
+		asm volatile (
+			"sbi %[port], %[bit] \n\t"				// Set the output bit
+			".rept %[onCycles] \n\t"				// Now timing actually matters. The 0-bit must be long enough to be detected but not too long or it will be a 1-bit
+			"nop \n\t"                                              // Execute NOPs to delay exactly the specified number of cycles
+			".endr \n\t"
+			"cbi %[port], %[bit] \n\t"                              // Clear the output bit
+			".rept %[offCycles] \n\t"                               // Execute NOPs to delay exactly the specified number of cycles
+			"nop \n\t"
+			".endr \n\t"
+			::
+			[port]		"I" (_SFR_IO_ADDR(PIXEL_PORT)),
+			[bit]		"I" (PIXEL_BIT),
+			[onCycles]	"I" (NS_TO_CYCLES(T0H) - 2),
+			[offCycles]	"I" (NS_TO_CYCLES(T0L) - 2)
+
+		);
+      
+    }
+    
+    // Note that the inter-bit gap can be as long as you want as long as it doesn't exceed the 5us reset timeout (which is A long time)
+    // Here I have been generous and not tried to squeeze the gap tight but instead erred on the side of lots of extra time.
+    // This has thenice side effect of avoid glitches on very long strings becuase 
+
+    
+}  
+
+  
+inline void sendByte( unsigned char byte ) {
+    
+    for( unsigned char bit = 0 ; bit < 8 ; bit++ ) {
+      
+      sendBit( bitRead( byte , 7 ) );                // Neopixel wants bit in highest-to-lowest order
+                                                     // so send highest bit (bit #7 in an 8-bit byte since they start at 0)
+      byte <<= 1;                                    // and then shift left so bit 6 moves into 7, 5 moves into 6, etc
+      
+    }           
+} 
+
+/*
+  The following three functions are the public API:
+  
+  ledSetup() - set up the pin that is connected to the string. Call once at the begining of the program.  
+  sendPixel( r g , b ) - send a single pixel to the string. Call this once for each pixel in a frame.
+  show() - show the recently sent pixel on the LEDs . Call once per frame. 
+  
+*/
+
+
+// Set the specified pin up as digital out
+
+void ledsetup() {
+  
+  bitSet( PIXEL_DDR , PIXEL_BIT );
+  
+}
+
+inline void sendPixel( unsigned char r, unsigned char g , unsigned char b )  {  
+  
+  sendByte(g);          // Neopixel wants colors in green then red then blue order
+  sendByte(r);
+  sendByte(b);
+  
+}
+
+
+// Just wait long enough without sending any bots to cause the pixels to latch and display the last sent frame
+
+void show() {
+	_delay_us( (RES / 1000UL) + 1);				// Round up since the delay must be _at_least_ this long (too short might not work, too long not a problem)
+}
+
+
+/*
+  That is the whole API. What follows are some demo functions rewriten from the AdaFruit strandtest code...
+  
+  https://github.com/adafruit/Adafruit_NeoPixel/blob/master/examples/strandtest/strandtest.ino
+  
+  Note that we always turn off interrupts while we are sending pixels becuase an interupt
+  could happen just when we were in the middle of somehting time sensitive.
+  
+  If we wanted to minimize the time interrupts were off, we could instead 
+  could get away with only turning off interrupts just for the very brief moment 
+  when we are actually sending a 0 bit (~1us), as long as we were sure that the total time 
+  taken by any interrupts + the time in our pixel generation code never exceeded the reset time (5us).
+  
+*/
+
+
+// Display a single color on the whole string
+
+void showColor( unsigned char r , unsigned char g , unsigned char b ) {
+  
+  cli();  
+  for( int p=0; p<PIXELS; p++ ) {
+    sendPixel( r , g , b );
+  }
+  sei();
+  show();
+  
+}
+
+// Fill the dots one after the other with a color
+// rewrite to lift the compare out of the loop
+void colorWipe(unsigned char r , unsigned char g, unsigned char b, unsigned  char wait ) {
+  for(unsigned int i=0; i<PIXELS; i+= (PIXELS/60) ) {
+    
+    cli();
+    unsigned int p=0;
+    
+    while (p++<=i) {
+        sendPixel(r,g,b);
+    } 
+     
+    while (p++<=PIXELS) {
+        sendPixel(0,0,0);  
+      
+    }
+    
+    sei();
+    show();
+    delay(wait);
+  }
+}
+
+// I rewrite this one from scrtach to use high resolution for the color wheel to look nicer on a *much* bigger string
+                                                                            
+void rainbowCycle(unsigned int frames , unsigned int frameAdvance, unsigned int pixelAdvance ) {
+  
+  // Hue is a number between 0 and 3*256 than defines a mix of r->g->b where
+  // hue of 0 = Full red
+  // hue of 128 = 1/2 red and 1/2 green
+  // hue of 256 = Full Green
+  // hue of 384 = 1/2 green and 1/2 blue
+  // ...
+  
+  unsigned int firstPixelHue = 0;     // Color for the first pixel in the string
+  
+  for(unsigned int j=0; j<frames; j++) {                                  
+    
+    unsigned int currentPixelHue = firstPixelHue;
+       
+    cli();    
+        
+    for(unsigned int i=0; i< PIXELS; i++) {
+      
+      if (currentPixelHue>=(3*256)) {                  // Normalize back down incase we incremented and overflowed
+        currentPixelHue -= (3*256);
+      }
+            
+      unsigned char phase = currentPixelHue >> 8;
+      unsigned char step = currentPixelHue & 0xff;
+                 
+      switch (phase) {
+        
+        case 0: 
+          sendPixel( ~step , step ,  0 );
+          break;
+          
+        case 1: 
+          sendPixel( 0 , ~step , step );
+          break;
+
+        case 2: 
+          sendPixel(  step ,0 , ~step );
+          break;
+          
+      }
+      
+      currentPixelHue+=pixelAdvance;                                      
+      
+                          
+    } 
+    
+    sei();
+    
+    show();
+    
+    firstPixelHue += frameAdvance;
+           
+  }
+}
+
+//############################################################################
+
+EthernetUDP udp;
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
+
+  Ethernet.begin(mac,IPAddress(192,168,0,6));
+
+  int success = udp.begin(5000);
+  
+  ledsetup();
+  
+  for (int i =0,b=0;i < 64;++i,++b) {
+   showColor(i,i,b/4);//slight yellow tinge from on.
+  delay(15); 
+  }
+  //Serial.println(success ? "success" : "failed");
+}
+
+int r=0,g=0,b=0;
+
+void loop() {
+
+  //check for new udp-packet:
+  int size = udp.parsePacket();
+  if (size > 0) {
+    do {
+      char* msg = (char*)malloc(size+1);
+      int len = udp.read(msg,size+1);
+      msg[len]=0;
+
+      char* sub = msg;
+      short int id = atoi(strtok(sub,":"));
+
+      char* mode = strtok(NULL,":");
+      r = atoi(strtok(NULL,":"));
+      g = atoi(strtok(NULL,":"));
+      b = atoi(strtok(NULL,":"));
+      if ( id == 0 ) {
+        if ( strcmp(mode,"colorWipe") == 0 ) {
+         colorWipe(r, g, b, atoi(strtok(NULL,":"))); //  
+        }
+        if ( strcmp(mode,"showColor") == 0 ) {
+         showColor(r, g, b); //  
+        }
+        if ( strcmp(mode,"rainbowCycle") == 0 ) {
+        rainbowCycle(r,g,b);
+        }
+      } else { //pass message onwards not for us.
+         String st = "";
+         st +=  id; 
+         st += ":";
+         st += mode; 
+         st += ":";
+         st += r;
+         st += ":";
+         st += g;
+         st += ":";
+         st += b;
+         st += "\n";
+         Serial.print(st);
+      }
+      free(msg);
+    } while ((size = udp.available())>0);
+    //finish reading this packet:
+    udp.flush();
+    int success;
+    do {
+        success = udp.beginPacket(udp.remoteIP(),udp.remotePort());
+    } while (!success);
+
+    success = udp.println("hello world from arduino");
+
+    //Serial.print("bytes written: ");
+    //Serial.println(success);
+
+    success = udp.endPacket();
+
+    //Serial.print("endPacket: ");
+    //Serial.println(success ? "success" : "failed");
+
+    udp.stop();
+    
+    //restart with new connection to receive packets from other clients
+    //Serial.print("restart connection: ");
+    //Serial.println (udp.begin(5000) ? "success" : "failed");
+    udp.begin(5000);  
+  }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Udp_ws2812_serial
 This project combinds the ethernt enc28j60 and the ws2812 led pixel lib to create a network for house lighting.
+
+The master Arduino sketch will accept commands over Ethernet. This can be done using netcat. The following command will control a string of lights connected to PIN number defined in the sketch. The netcat command is sending a udp raw udb packet over the ethernet. I used a server to do this. The string is built around the following.
+
+
+	"echo -n "0:showColor:0:100:100" | nc -4u -w1 192.168.0.6 5000"
+
+1. The 0 is the id. This was so one can string multiple arduinos together via serial and respond to its own id this is a short int.
+
+2. The "showColor" is a string that definds a command. These can be extendeed.
+
+3. The sections after this are numbers associated with the command. In this case its red green and blue.
+
+So far I have implimented showColour and rainbowCycle. The three numbers that accompanies this command follow the Neopixel API. 
+
+	0:rainbowCycle:1000:10:5
+
+the 1000 specifies a duration this is about 10 seconds. 10 is the speed as which the rainbow progresses and 5 is the nunber of colour spectrums will be squeezed onto the light strip.    
